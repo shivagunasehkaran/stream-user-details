013e473c1481ac3e12f640e414679a347b9952ee
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-23 01:45:47 +0530

    Overlay component tested

---
+++ __tests__/components/OverLay.test.js
@@ -0,0 +1,18 @@
+import enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import OverLay from '../../src/components/OverLay/OverLay';
+
+enzyme.configure({adapter: new Adapter()});
+
+describe('Overlay component validation', () => {
+  it('renders correctly with Overlay detail elments', async () => {
+    const wrapper = shallow(<OverLay />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render with text elements', async () => {
+    const wrapper = shallow(<OverLay />);
+    expect(wrapper.find('Text')).toHaveLength(4);
+  });
+});

fe5a7d94dcdaebad042c2018603c2a6aa9ab4831
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-07-22 14:34:13 +0530

    Sample UI creation in App file

---
+++ src/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = (props: Props) => {
+  return (
+    <View style={{flex: 1}}>
+      <SafeAreaView>
+        <Text>{'Hi Shiva'}</Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default App;
